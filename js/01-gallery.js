8582073544f03a392677304f746734ec66536c4d
AntonBezradnii <bezanton@ukr.net>
2023-04-02 20:33:57 +0300

    done HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const ulListEl = document.querySelector('.gallery');
+
+const listItemImgMarkup = galleryItems.map(image =>
+    `<li class="gallery__item">
+        <a href="${image.original}" class="gallery__link">
+            <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+            />
+        </a>
+    </li>`
+).join('');
+
+ulListEl.innerHTML = listItemImgMarkup;
+
+ulListEl.addEventListener('click', onImgClick);
+
+
+function onImgClick(event) {
+    event.preventDefault();
+
+    const clickOnImg = event.target.classList.contains('gallery__image');
+
+    if (!clickOnImg) {
+        return
+    } 
+        const instance = basicLightbox.create(`
+    <img
+        src="${event.target.dataset.source}"
+        alt="${event.target.alt}"
+    />`
+    );
+    instance.show();
+    
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const ulListEl = document.querySelector('.gallery');
+
+const listItemImgMarkup = galleryItems.map(image =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+        </a>
+    </li>`
+).join('');
+
+ulListEl.innerHTML = listItemImgMarkup;
+
+// ulListEl.addEventListener('click', onImgClick);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
